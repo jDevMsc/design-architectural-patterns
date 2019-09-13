2fb82eb673e7178e3c6856b7d568f293084699ac
Artem Luzan <unlimitet2011@yandex.ru>
2019-09-13 14:08:45 +0300

    Singleton [vers1]

---
+++ factory-method/src/main/java/com/Singleton/ProgramRuner.java
@@ -0,0 +1,23 @@
+package com.Singleton;
+
+public class ProgramRuner  {
+
+  public static void main(String[] args) {
+
+    ProgramLogger programLogger = ProgramLogger.getProgramLogger();
+
+    programLogger.addLogInfo("First log..");
+    programLogger.addLogInfo("Second log..");
+    programLogger.addLogInfo("Third log..");
+
+    ProgramLogger programLogger2 = ProgramLogger.getProgramLogger();//всегда возвращается один и тот же объект
+
+
+    programLogger2.showLogFile();
+
+
+
+
+  }
+
+}
