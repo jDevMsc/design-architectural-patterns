468029dc61d3061eedea0a21028247dac37c84f9
Artem Luzan <unlimitet2011@yandex.ru>
2019-09-21 17:35:16 +0300

    Chain_of_command [vers1]

---
+++ factory-method/src/main/java/com/chain_of_command/vers1/EmailNotifier.java
@@ -0,0 +1,13 @@
+package com.chain_of_command.vers1;
+
+public class EmailNotifier extends Notifier {
+
+  public EmailNotifier(int priority) {
+    super(priority);
+  }
+
+  @Override
+  public void write(String message) {
+    System.out.println("Sending email:" + message);
+  }
+}
