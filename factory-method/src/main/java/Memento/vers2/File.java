252266a728dd175a24d020dc47eaa62383a8a9e1
Artem Luzan <unlimitet2011@yandex.ru>
2019-09-25 18:24:31 +0300

    Memento [Хранитель] version 2

---
+++ factory-method/src/main/java/Memento/vers2/File.java
@@ -0,0 +1,14 @@
+package Memento.vers2;
+
+public class File {
+
+  Save save;
+
+  public Save getSave() {
+    return save;
+  }
+
+  public void setSave(Save save) {
+    this.save = save;
+  }
+}
---
+++ factory-method/src/main/java/Memento/vers2/MementoGame.java
@@ -0,0 +1,21 @@
+package Memento.vers2;
+
+public class MementoGame {
+
+  public static void main(String[] args) {
+  Game game = new Game();
+  game.set("LVL1", 3000);
+  System.out.println(game);
+
+  File file = new File();
+  file.setSave(game.save());
+
+    game.set("LVL2", 55000);
+    System.out.println(game);
+
+    game.load(file.getSave());
+    System.out.println(game);
+
+
+  }
+}
