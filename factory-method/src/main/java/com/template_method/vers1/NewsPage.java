8896b431d1d69d71e6a1efc68b957e4942f67d3d
Artem Luzan <unlimitet2011@yandex.ru>
2019-10-02 13:26:49 +0300

    Template Method [Шаблонный метод] version 1

---
+++ factory-method/src/main/java/com/template_method/vers1/NewsPage.java
@@ -0,0 +1,9 @@
+package com.template_method.vers1;
+
+public class NewsPage extends WebSiteTemplate {
+  @Override
+  public void showPageContent() {
+    System.out.println("News");
+  }
+
+}
---
+++ factory-method/src/main/java/com/template_method/vers1/WelcomePage.java
@@ -0,0 +1,11 @@
+package com.template_method.vers1;
+
+public class WelcomePage extends WebSiteTemplate {
+
+
+
+  @Override
+  public void showPageContent() {
+    System.out.println("Welcome");
+  }
+}
